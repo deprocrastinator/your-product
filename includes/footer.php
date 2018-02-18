d5ce5c3a2f24b8ea99dda4eed54cf77c1cdd0ecb
Deividas Šimkus <deividas.simkus@mif.stud.vu.lt>
2018-02-18 20:28:41 +0200

    clean up

--- includes/footer.php
+++
@@ -1,6 +0,0 @@
-    <div class="copyright">
-        &copy; 2018 Chair Inc. All rights reserved.</br>
-    </div>
-
-</body>
-</html>
--- views/posts/show.php
+++
@@ -1,7 +0,0 @@
-<p>This is the requested order:</p>
-
-<p><?php echo $order->id; ?></p>
-<p><?php echo $order->quantity; ?></p>
-<p><?php echo $order->firstname; ?></p>
-<p><?php echo $order->lastname; ?></p>
-<p><?php echo $order->email; ?></p>
